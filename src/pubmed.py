1c8c850659057b54d022b2f38b3943a6d66a6bf9
wangtao <wangtao.cpu@gmail.com>
2025-06-07 20:54:19 +0800

    创建项目

---
+++ src/pubmed.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# Create Date: 2025/06/07
+# Author: wangtao <wangtao.cpu@gmail.com>
+# File Name: pubmed.py
+# Description: 获取 PubMed 数据
+
+from typing import Optional, Literal
+from dataclasses import dataclass
+
+import requests
+from bs4 import BeautifulSoup
+from loguru import logger
+
+from type import Markdown
+
+
+@dataclass
+class Article:
+    url: str
+    doi: str
+    title: str
+    abstract: Markdown
+    keywords: Optional[list[str]]
+
+
+def get_article(url: str) -> Optional[Article]:
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    soup = BeautifulSoup(response.text, "html.parser")
+    title = soup.find("h1", class_="heading-title").text.strip()
+    doi = soup.find("span", class_="identifier doi").find("a").text.strip()
+    abstract_content = soup.find("div", id="abstract")
+    abstract = Markdown.from_html(str(abstract_content.find("div", class_="abstract-content")))
+    if ps := abstract_content.find("p", recursive=False):
+        keywords = [s.strip() for s in ps.text.replace("Keywords:", "").split(";")]
+    else:
+        keywords = None
+    return Article(url, doi, title, abstract, keywords)
+
+
+def search_article_urls(term: str, recent_year: Optional[Literal[1, 2, 5]] = None, max_count = None, **kwargs) -> Optional[list[str]]:   
+    logger.info(f"search article urls: {term}")  
+    base_url = f"https://pubmed.ncbi.nlm.nih.gov/?term={term}"
+    if recent_year:
+        base_url += f"&filter=datesearch.y_{recent_year}"
+    for k, v in kwargs:
+        base_url += f"&{k}={v}"
+    
+    page = 1
+    article_urls = []
+    while True:
+        url = f"{base_url}&page={page}"
+        response = requests.get(url)
+        if response.status_code != 200:
+            return None
+        soup = BeautifulSoup(response.text, "html.parser")
+        articles = soup.find_all("article")
+        if not articles:
+            break
+        logger.info(f"page {page} found {len(articles)} articles")
+        for article in articles:
+            article_urls.append("https://pubmed.ncbi.nlm.nih.gov" + article.find("a")["href"])
+        if max_count and len(article_urls) >= max_count:
+            article_urls = article_urls[:max_count]
+            logger.info(f"max_count reached")
+            break
+        page += 1
+
+    logger.success(f"return {len(article_urls)} article urls")
+    return article_urls
+
+
+if __name__ == "__main__":
+    pass
---
+++ src/type.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Create Date: 2025/06/07
+# Author: wangtao <wangtao.cpu@gmail.com>
+# File Name: types.py
+# Description: 中间类型
+
+from typing import Any
+
+import html2text
+
+
+class Markdown:
+    
+    html2md = html2text.HTML2Text()
+    html2md.ignore_links = True
+    html2md.ignore_images = True
+    html2md.ignore_tables = True
+    html2md.body_width = 0
+    
+    def __init__(self, markdown: str) -> None:
+        self.markdown = markdown
+    
+    @classmethod
+    def from_html(cls, html: Any) -> "Markdown":
+        return cls(cls.html2md.handle(str(html)).strip())
+
+    def __str__(self) -> str:
+        return self.markdown
+    
+    def __repr__(self) -> str:
+        return self.markdown
+    
